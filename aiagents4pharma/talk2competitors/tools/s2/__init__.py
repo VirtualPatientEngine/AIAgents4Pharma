67cdc30f7d5cad063b4c29b0eba64ee54e32ebb1
Ansh-info <anshkumar.info@gmail.com>
2025-01-21 23:38:25 +0100

    feat: initialize Semantic Scholar tools package

---
+++ aiagents4pharma/talk2competitors/tools/s2/__init__.py
@@ -0,0 +1,20 @@
+from tools.s2.display_results import display_results
+from tools.s2.multi_paper_rec import get_multi_paper_recommendations
+from tools.s2.search import search_tool
+from tools.s2.single_paper_rec import get_single_paper_recommendations
+
+# Export all tools in a list for easy access
+s2_tools = [
+    search_tool,
+    display_results,
+    get_single_paper_recommendations,
+    get_multi_paper_recommendations,
+]
+
+__all__ = [
+    "search_tool",
+    "display_results",
+    "get_single_paper_recommendations",
+    "get_multi_paper_recommendations",
+    "s2_tools",
+]
