e74760705327e07791e6b39c180fbf00eae79b65
gurdeep330 <gurdeep330@gmail.com>
2025-02-05 16:36:49 +0100

    fix: tests

--- aiagents4pharma/talk2biomodels/tests/test_query_article.py
+++ aiagents4pharma/talk2biomodels/tests/test_query_article.py
@@ -3,7 +3,6 @@ Test cases for Talk2Biomodels query_article tool.
 '''
 
 from pydantic import BaseModel, Field
-from langchain_openai import ChatOpenAI
 from langchain_core.messages import HumanMessage, ToolMessage
 from ..agents.t2b_agent import get_app
 
@@ -31,18 +30,11 @@ def test_query_article_with_an_article():
                     )
     # Get the response from the tool
     assistant_msg = response["messages"][-1].content
-    # Create a LLM model
-    llm = ChatOpenAI(model='gpt-4o-mini', temperature=0)
-    # Add structured output to the LLM model
-    structured_llm = llm.with_structured_output(Article)
-    # Ask the LLM model to predict the title of the article
-    # from the response of the query_article tool
-    prompt = "Given the text below, what is the title of the article?\n\n" + assistant_msg
-    article = structured_llm.invoke(prompt)
     # Check if the article title is correct
     expected_title = "A Multiscale Model of IL-6–Mediated "
     expected_title += "Immune Regulation in Crohn’s Disease"
-    assert article.title == expected_title
+    assert expected_title in assistant_msg
+    # assert article.title == expected_title
 
 def test_query_article_without_an_article():
     '''
