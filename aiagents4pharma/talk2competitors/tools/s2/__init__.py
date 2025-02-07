55755ab81467e3f23d1448d8ff0dd1e06bf63f89
Ansh-info <anshkumar.info@gmail.com>
2025-02-07 22:15:38 +0100

    chores: updated agent name

--- aiagents4pharma/talk2competitors/tools/s2/__init__.py
+++
@@ -1,8 +0,0 @@
-'''
-This file is used to import all the modules in the package.
-'''
-
-from . import display_results
-from . import multi_paper_rec
-from . import search
-from . import single_paper_rec
