5bd3bb156f64f4cbf1d7cd4d4c72ba69d5d53c9c
Ansh-info <anshkumar.info@gmail.com>
2025-02-10 19:34:37 +0100

    feat: error raised in case of now papers found

--- aiagents4pharma/talk2scholars/tools/s2/display_results.py
+++ aiagents4pharma/talk2scholars/tools/s2/display_results.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
-'''
+"""
 This tool is used to display the table of studies.
-'''
+"""
 
 import logging
 from typing import Annotated
@@ -13,13 +13,25 @@ from langgraph.prebuilt import InjectedState
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-@tool('display_results')
+
+@tool("display_results")
 def display_results(state: Annotated[dict, InjectedState]):
     """
     Display the papers in the state.
 
     Args:
-        state (dict): The state of the agent.
+        state (dict): The state of the agent containing the papers.
+
+    Returns:
+        dict: A dictionary containing the papers and multi_papers from the state.
+
+    Raises:
+        ValueError: If no papers are found in the state, trigger a search.
     """
     logger.info("Displaying papers from the state")
-    return state["papers"]
+
+    if not state.get("papers") and not state.get("multi_papers"):
+        logger.error("No papers found in the state. Triggering a search.")
+        raise ValueError("No papers found in the state. Please trigger a search.")
+
+    return {"papers": state["papers"], "multi_papers": state["multi_papers"]}
