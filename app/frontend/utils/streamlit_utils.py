d7152e83958dcdb9c909c7214950aaa8f2f3cca7
Ansh-info <anshkumar.info@gmail.com>
2025-04-29 02:15:17 +0200

    chores: update starter questions

--- app/frontend/utils/streamlit_utils.py
+++ app/frontend/utils/streamlit_utils.py
@@ -209,7 +209,6 @@ def sample_questions_t2s():
     """
     questions = [
         'Search articles on "Role of DNA damage response (DDR) in Cancer"',
-        "Tell me more about the first article in the last search results",
         "Save these articles in my Zotero library under the collection 'Curiosity'",
         "Download the article 'BioBridge' with arxiv_id '2310.03320' and summarize it",
         "First, show all the papers in my Zotero library. Then, for each paper, list the PDB IDs of the 3D structures of the GPCRs used in the PDFs.",
