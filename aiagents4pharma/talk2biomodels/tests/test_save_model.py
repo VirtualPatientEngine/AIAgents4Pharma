9bac6dd347f5b4f937225bc48eb0e802c0c9b020
gurdeep330 <gurdeep330@gmail.com>
2025-09-25 22:10:45 +0200

    fix: update save_model test

--- aiagents4pharma/talk2biomodels/tests/test_save_model.py
+++ aiagents4pharma/talk2biomodels/tests/test_save_model.py
@@ -2,6 +2,8 @@
 Test cases for Talk2Biomodels.
 """
 
+import tempfile
+
 from langchain_core.messages import HumanMessage
 from langchain_openai import ChatOpenAI
 
@@ -23,42 +25,23 @@ def test_save_model_tool():
     app.invoke({"messages": [HumanMessage(content=prompt)]}, config=config)
     current_state = app.get_state(config)
     assert current_state.values["model_as_string"][-1] is not None
-    # Save a model
+    # Save a model without simulating
     prompt = "Save the model"
     # Invoke the agent
     app.invoke({"messages": [HumanMessage(content=prompt)]}, config=config)
     current_state = app.get_state(config)
     assert current_state.values["model_as_string"][-1] is not None
-    # Simulate and save a model
-    prompt = "Simulate model 64 and then save the model at /home/gsingh/"
-    # Invoke the agent
-    app.invoke({"messages": [HumanMessage(content=prompt)]}, config=config)
-    current_state = app.get_state(config)
-    assert current_state.values["model_as_string"][-1] is not None
-    # Simulate and ave a model
+    # Create a temporary directory to save the model
+    with tempfile.TemporaryDirectory() as temp_dir:
+        # Save a model to the temporary directory
+        prompt = f"Simulate model 64 and save it model at {temp_dir}"
+        # Invoke the agent
+        app.invoke({"messages": [HumanMessage(content=prompt)]}, config=config)
+        current_state = app.get_state(config)
+        assert current_state.values["model_as_string"][-1] is not None
+    # Simulate and save a model in non-existing path
     prompt = "Simulate model 64 and then save the model at /xyz/"
     # Invoke the agent
     app.invoke({"messages": [HumanMessage(content=prompt)]}, config=config)
     current_state = app.get_state(config)
     assert current_state.values["model_as_string"][-1] is not None
-    # Simulate and ave a model
-    prompt = "Simulate model 64 and then save the model at None"
-    # Invoke the agent
-    app.invoke({"messages": [HumanMessage(content=prompt)]}, config=config)
-    current_state = app.get_state(config)
-    assert current_state.values["model_as_string"][-1] is not None
-    # dic_simulated_data = current_state.values["dic_simulated_data"]
-    # # Check if the dic_simulated_data is a list
-    # assert isinstance(dic_simulated_data, list)
-    # # Check if the length of the dic_simulated_data is 2
-    # assert len(dic_simulated_data) == 2
-    # # Check if the source of the first model is 64
-    # assert dic_simulated_data[0]["source"] == 64
-    # # Check if the source of the second model is upload
-    # assert dic_simulated_data[1]["source"] == "upload"
-    # # Check if the data of the first model contains
-    # assert "1,3-bisphosphoglycerate" in dic_simulated_data[0]["data"]
-    # # Check if the data of the second model contains
-    # assert "mTORC2" in dic_simulated_data[1]["data"]
-    # # Check if the model_as_string is not None
-    # assert current_state.values["model_as_string"][-1] is not None
